5de2f6eeaff8c054b0dfe32db1680d77eff78fbe
tracer-ics2020 <tracer@njuics.org>
2020-11-05 15:48:08 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:48:08 up 1 day, 21:30,  6 users,  load average: 1.39, 1.26, 1.17

--- nemu/src/isa/x86/exec/control.h
+++ nemu/src/isa/x86/exec/control.h
@@ -24,7 +24,9 @@ static inline def_EHelper(jmp_rm) {
 
 static inline def_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  //TODO();
+  rtl_j(s, s->jmp_pc);
+
   print_asm("call %x", s->jmp_pc);
 }
 
