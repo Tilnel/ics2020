f286653932d9d3d0c43525f19d42480997da4e89
tracer-ics2020 <tracer@njuics.org>
2020-11-05 15:33:47 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:33:47 up 1 day, 21:16,  6 users,  load average: 1.36, 1.14, 1.02

--- nemu/src/isa/x86/local-include/decode.h
+++ nemu/src/isa/x86/local-include/decode.h
@@ -47,14 +47,16 @@ static inline def_DopHelper(I) {
 /* sign immediate */
 static inline def_DopHelper(SI) {
   assert(op->width == 1 || op->width == 4);
-
+  word_t imm = instr_fetch(&s -> seq_pc, op->width);
+  if (op -> width == 1 && imm >> 7) imm = imm | 0xffffff00;
+  operand_imm(s, op, load_val, imm, op->width);
   /* TODO: Use instr_fetch() to read `op->width' bytes of memory
    * pointed by 's->seq_pc'. Interpret the result as a signed immediate,
    * and call `operand_imm()` as following.
    *
    operand_imm(s, op, load_val, ???, op->width);
    */
-  TODO();
+//  TODO();
 }
 
 /* I386 manual does not contain this abbreviation.
