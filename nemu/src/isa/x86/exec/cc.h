bcf9c704c3f44424afe8b9e3c11d494ed29a4c08
tracer-ics2020 <tracer@njuics.org>
2020-11-07 03:05:49 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:05:49 up 3 days,  8:48,  6 users,  load average: 1.39, 1.32, 1.23

--- nemu/src/isa/x86/exec/cc.h
+++ nemu/src/isa/x86/exec/cc.h
@@ -30,15 +30,23 @@ static inline void rtl_setcc(DecodeExecState *s, rtlreg_t* dest, uint32_t subcod
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O: *dest = (cpu.OF == 0)? 1 : 0; break;
+    case CC_NO: *dest = (cpu.OF == 1)? 1 : 0; break;
     case CC_B: *dest = (cpu.CF == 1)? 1 : 0; break;
+    case CC_NB: *dest = (cpu.CF == 0)? 1 : 0; break;
     case CC_E: *dest = (cpu.ZF == 1)? 1 : 0; break;
+    case CC_NE: *dest = (cpu.ZF == 0)? 1 : 0; break;
     case CC_BE: *dest = (cpu.CF == 1 || cpu.ZF == 1)? 1 : 0; break;
+    case CC_NBE: *dest = (cpu.CF == 1 || cpu.ZF == 1)? 0 : 1; break;
     case CC_S: *dest = (cpu.SF == 1)? 1 : 0; break;
+    case CC_NS: *dest = (cpu.SF == 0)? 1 : 0; break;
     case CC_L: *dest = (cpu.SF != cpu.OF)? 1 : 0; break;
+    case CC_NL: *dest = (cpu.SF == cpu.OF)? 1 : 0; break;
     case CC_LE: *dest = (cpu.ZF == 1 || cpu.SF != cpu.OF)? 1 : 0; break;
+    case CC_NLE: *dest = (cpu.ZF == 1 || cpu.SF != cpu.OF)? 0 : 1; break;
        //TODO();
     default: panic("should not reach here"); break;
     case CC_P: panic("PF is not supported"); break;
+    case CC_NP: panic("PF is not supported"); break;
   }
 
   if (invert) {
