3efd15ece0df5899e3d28b39ba86f7b2481e9fb6
tracer-ics2020 <tracer@njuics.org>
2020-11-07 02:15:34 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:15:34 up 3 days,  7:58,  6 users,  load average: 1.44, 1.30, 1.21

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -6,7 +6,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < 8; i++) {
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
-      printf("%x %x\n", ref_r->gpr[i]._32, cpu.gpr[i]._32);return false;}
+      printf("%d %x %x\n", i, ref_r->gpr[i]._32, cpu.gpr[i]._32);return false;}
   }
   if ((*ref_r).pc != cpu.pc) return false;
   return true;
