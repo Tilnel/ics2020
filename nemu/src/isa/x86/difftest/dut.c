a82525d236a9a81041f80f8a9af1e54247085dac
tracer-ics2020 <tracer@njuics.org>
2020-11-07 22:02:54 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:02:54 up 4 days,  3:45,  7 users,  load average: 1.59, 1.18, 1.07

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -4,15 +4,15 @@
 #include "difftest.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  printf("pc: %x %x  ", ref_r->pc, cpu.pc);
-  printf("ebp: %x %x", ref_r->ebp, cpu.ebp);
-  printf("eax: %x %x", ref_r->eax, cpu.eax);
+  // printf("pc: %x %x  ", ref_r->pc, cpu.pc);
+  // printf("ebp: %x %x", ref_r->ebp, cpu.ebp);
+  // printf("eax: %x %x", ref_r->eax, cpu.eax);
   for (int i = 0; i < 8; i++) {
     if (ref_r->gpr[i]._32 != cpu.gpr[i]._32) {
       printf("%d %x %x\n", i, ref_r->gpr[i]._32, cpu.gpr[i]._32);return false;}
   }
   //if ((*ref_r).pc != cpu.pc) return false;
-  printf("   edx: %x %x %x\n", ref_r->edx, cpu.edx, cpu.esp);
+  // printf("   edx: %x %x %x\n", ref_r->edx, cpu.edx, cpu.esp);
   return true;
 }
 
