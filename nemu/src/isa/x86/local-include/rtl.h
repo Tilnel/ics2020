90dcb74e2f00fd1e5b40e7a1e1506f6da22a2d43
tracer-ics2020 <tracer@njuics.org>
2020-11-07 12:59:24 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:59:24 up 3 days, 18:42,  6 users,  load average: 1.34, 1.40, 1.30

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -93,7 +93,8 @@ static inline def_rtl(update_ZF, const rtlreg_t* result, int width) {
 static inline def_rtl(update_SF, const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   width = 4;
-  *t0 = ((*result) >> (width * 8 - 1)) & 1;
+  *s1 = *result;
+  *t0 = ((*s1) >> (width * 8 - 1)) & 1;
   rtl_set_SF(s, t0);
   assert(!cpu.SF);
   assert(!(*t0));
