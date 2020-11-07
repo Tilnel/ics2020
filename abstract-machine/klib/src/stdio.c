226b69f1b83696e38f960f125e337407f0288855
tracer-ics2020 <tracer@njuics.org>
2020-11-08 04:36:11 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:36:11 up 4 days, 10:19,  7 users,  load average: 1.25, 1.29, 1.24

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,75 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
-}
+int printf(const char *fmt, ...) { return 0; }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+int vsprintf(char *out, const char *fmt, va_list ap) { return 0; }
+
+int itoa(int n, char *s) {
+    int i, j, sign;
+    char buf[12];
+    if ((sign = n) < 0)
+        n = -n;
+    i = 0;
+    do {
+        buf[i] = n % 10 + '0';
+        i++;
+    } while ((n /= 10) > 0);
+    if (sign < 0) {
+        s[i] = '-';
+        i++;
+    }
+
+    for (j = i - 1; j >= 0; j--) {
+        s[j] = buf[i - 1 - j];
+    }
+    s[i] = '\0';
+    return i;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    va_list ap;
+    size_t i = 0;
+    int d;
+    char *s;
+    size_t pos = 0;
+    size_t len;
+    va_start(ap, fmt);
+    while (fmt[i] != '\0') {
+        switch (fmt[i]) {
+        case '%':
+            switch (fmt[++i]) {
+            case 's':
+                s = va_arg(ap, char *);
+                len = strlen(s);
+                strncpy(out + pos, s, len);
+                pos += len;
+                break;
+            case 'd':
+                d = va_arg(ap, int);
+                len = itoa(d, out + pos);
+                pos += len;
+                break;
+            }
+            break;
+
+        default:
+            out[pos] = fmt[i];
+            pos++;
+        }
+        i++;
+    }
+    out[pos] = '\0';
+    va_end(ap);
+    return 0;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
-}
+int snprintf(char *out, size_t n, const char *fmt, ...) { return 0; }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  return 0;
-}
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) { return 0; }
 
 #endif
