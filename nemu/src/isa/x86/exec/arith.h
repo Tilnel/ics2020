f0194381d8c7129c565a11169bb2a46eeafd4d32
tracer-ics2020 <tracer@njuics.org>
2020-11-07 13:09:29 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:09:29 up 3 days, 18:52,  6 users,  load average: 1.46, 1.45, 1.35

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -37,12 +37,12 @@ static inline def_EHelper(cmp) {
   *s1 = *ddest;
   rtl_sext(s, s2, dsrc1, id_src1->width);
   *s1 -= *s2;
+  rtl_update_ZFSF(s, s1, id_dest->width);
   rtl_is_sub_carry(s, s0, ddest, s2);
   assert(!(*s0));
   cpu.CF = *s0; assert(!cpu.CF);
   rtl_is_sub_overflow(s, s0, s1, ddest, s2, id_dest->width);
   cpu.OF = *s0; assert(!cpu.OF);
-  rtl_update_ZFSF(s, s1, id_dest->width);
   //TODO();
   print_asm_template2(cmp);
 }
