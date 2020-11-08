56cdeb1824d3703e637f1d1b66457349ad5d713e
tracer-ics2020 <tracer@njuics.org>
2020-11-08 15:59:53 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:59:54 up 4 days, 21:42,  8 users,  load average: 1.11, 1.15, 1.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -49,7 +49,7 @@ int sprintf(char *out, const char *fmt, ...) {
     int d;
     char *s;
     size_t pos = 0;
-    size_t len;
+    size_t len, addlen;
     va_start(ap, fmt);
     while (fmt[i] != '\0') {
         switch (fmt[i]) {
@@ -60,6 +60,19 @@ int sprintf(char *out, const char *fmt, ...) {
                 len = strlen(s);
                 strncpy(out + pos, s, len);
                 pos += len;
+                break;
+            case '0':
+                addlen = fmt[++i] - '0';
+                d = va_arg(ap, int);
+                len = itoa(d, out + pos);
+                addlen -= len;
+                while (addlen--) {
+                  out[pos] = '0';
+                  pos++;
+                }
+                itoa (d, out + pos);
+                pos += len;
+                i++;
                 break;
             case 'd':
                 d = va_arg(ap, int);
