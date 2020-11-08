a32ee866b86e39022aeb274f1b8e2e6064edb5a6
tracer-ics2020 <tracer@njuics.org>
2020-11-08 08:40:18 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:40:18 up 4 days, 14:23,  8 users,  load average: 1.35, 1.45, 1.39

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -13,7 +13,7 @@ static inline void set_width(DecodeExecState *s, int width) {
 /* 0x80, 0x81, 0x83 */
 static inline def_EHelper(gp1) {
   switch (s->isa.ext_opcode) {
-    EX(0, addl) EMPTY(1) EMPTY(2) EMPTY(3)
+    EX(0, addl) EX(1, or) EMPTY(2) EMPTY(3)
     EX(4, and) EX(6, xor) EX(7, cmp)
     EX(5, sub)
   }
