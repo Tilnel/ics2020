41ab614f66ddcbc569d7a78fbe3d73ca7e68e9fd
tracer-ics2020 <tracer@njuics.org>
2020-11-08 16:24:12 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:24:13 up 4 days, 22:07,  8 users,  load average: 1.58, 1.47, 1.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,13 +7,13 @@
 
 int printf(const char *fmt, ...) {
 
-  assert(0);
   va_list ap;
   va_start(ap, fmt);
   char buf[256];
   sprintf(buf, fmt, ap);
   va_end(ap);
   size_t i = 0;
+assert(0);
   assert(buf[i] != '\0');
   while (buf[i] != '\0') {
     putch(buf[i]);
