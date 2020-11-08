8c0901a7f3cfa4f70a15c4c7c711663b0d946128
tracer-ics2020 <tracer@njuics.org>
2020-11-08 16:51:31 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:51:32 up 4 days, 22:34,  8 users,  load average: 1.05, 1.38, 1.49

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,7 +10,6 @@ int printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   sprintf(buf, fmt, ap);
-  assert(0);
   va_end(ap);
   size_t i = 0;
   while (buf[i] != '\0') {
@@ -50,7 +49,7 @@ int sprintf(char *out, const char *fmt, ...) {
     int d;
     char *s;
     size_t pos = 0;
-    size_t len;//, addlen;
+    size_t len, addlen;
     va_start(ap, fmt);
     while (fmt[i] != '\0' && i < 256) {
         switch (fmt[i]) {
@@ -61,8 +60,9 @@ int sprintf(char *out, const char *fmt, ...) {
                 len = strlen(s);
                 strncpy(out + pos, s, len);
                 pos += len;
+                assert(0);
                 break;
-            /*case '0':
+            case '0':
                 addlen = fmt[i + 1] - '0';
                 d = va_arg(ap, int);
                 len = itoa(d, out + pos);
@@ -75,7 +75,7 @@ int sprintf(char *out, const char *fmt, ...) {
                 itoa (d, out + pos);
                 pos += len;
                 i+=2;
-                break;*/
+                break;
             case 'd':
                 d = va_arg(ap, int);
                 len = itoa(d, out + pos);
