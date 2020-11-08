6e44dc2817864fe73942bab6ff71c52641434eaa
tracer-ics2020 <tracer@njuics.org>
2020-11-08 15:34:47 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3e71520613764e38a08ef6bd88b294e8   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:34:47 up 4 days, 21:17,  8 users,  load average: 1.28, 1.42, 1.38

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -5,7 +5,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = inl(RTC_ADDR) + 1000000 * inl(RTC_ADDR + 4);
+  uptime->us = inl(RTC_ADDR + 4) + 1000000 * inl(RTC_ADDR);
 
 
 }
