0c965905068758126f9173e1ed5477d8aff4a5e4
tracer-ics2020 <tracer@njuics.org>
2020-11-10 02:23:25 +0800

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 885c3adfe64a465cbe3fc5e8545bfad0   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:23:25 up  4:36,  4 users,  load average: 0.67, 0.45, 0.44

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -30,7 +30,7 @@ static inline def_EHelper(gp2) {
 /* 0xf6, 0xf7 */
 static inline def_EHelper(gp3) {
   switch (s->isa.ext_opcode) {
-    IDEX(0, test_I, test) EMPTY(1) EX(2, not) EX(3, neg)
+    IDEXW(0, test_I, test, id_dest->width) EMPTY(1) EX(2, not) EX(3, neg)
     EX(4, mul) EX(5, imul1) EX(6, div) EX(7, idiv)
   }
 }
