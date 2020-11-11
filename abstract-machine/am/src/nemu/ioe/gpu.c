e81778a5236ee05a295c095a87c227102e363564
tracer-ics2020 <tracer@njuics.org>
2020-11-11 07:27:41 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 706ffcc8c6fc42e6b263f0bfd67699d7   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:27:42 up  7:30,  8 users,  load average: 1.46, 1.38, 1.20

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -16,7 +16,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 400, .height = 300,
+    .width = 0, .height = 0,
     .vmemsz = 0
   };
 }
