5b0563e8f7f93e405896f9da652e4ec03992f653
tracer-ics2020 <tracer@njuics.org>
2020-11-11 18:01:23 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 706ffcc8c6fc42e6b263f0bfd67699d7   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:01:23 up  5:04,  8 users,  load average: 1.99, 1.65, 1.30

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
+  if(kbd->keydown == 0)
   kbd->keycode = AM_KEY_NONE;
+  else kbd->keycode = inl(KBD_ADDR);
 }
