20b71aad07690762d3c34041c378465bf4a73bb1
tracer-ics2020 <tracer@njuics.org>
2020-11-11 19:09:25 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 706ffcc8c6fc42e6b263f0bfd67699d7   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  19:09:25 up  6:12,  8 users,  load average: 1.33, 1.17, 1.15

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -18,7 +18,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  /*int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   uint32_t *pixels = ctl->pixels;
   for (int i = 0; i < h && y + i < 300; i ++) {
     for (int j = 0; j < w && x + j < 400; j ++) {
@@ -26,7 +26,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       pixels ++;
     }
     pixels += w;
-  }
+  }*/
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
