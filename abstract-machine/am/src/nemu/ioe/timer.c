7200b0aa60305bdc216e1b66f35e7eb75f88fa14
tracer-ics2020 <tracer@njuics.org>
2020-11-11 10:30:10 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 885c3adfe64a465cbe3fc5e8545bfad0   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:30:10 up 1 day, 12:42, 14 users,  load average: 2.02, 2.01, 1.77

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 uint64_t UPTIME;
 void __am_timer_init() {
-  UPTIME = inl(RTC_ADDR) + inl(RTC_ADDR) * 1000000;
+  UPTIME = inl(RTC_ADDR) + inl(RTC_ADDR + 4) * 1000000;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = inl(RTC_ADDR) + inl(RTC_ADDR) * 1000000 - UPTIME;
+  uptime->us = inl(RTC_ADDR) + inl(RTC_ADDR + 4) * 1000000 - UPTIME;
   // ioe_write(AM_TIMER_RTC, NULL);
 }
 
