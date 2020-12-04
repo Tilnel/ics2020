daf83c9d74022f6051774eaa03ff28a3cd32a62e
tracer-ics2020 <tracer@njuics.org>
2020-12-04 11:36:11 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: bbf301b576964dffaec00ab6d784a22b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:36:11 up 3 days, 16:56,  6 users,  load average: 0.87, 1.16, 1.14

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -23,32 +23,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  // int W = 400, H = 300;
-  /*
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  uint32_t *pixels = ctl->pixels;
-  int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-  for (int j = 0; j < h && y + j < H; j ++) {
-    memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
-    pixels += w;
-  }
-  */
   int H = inw(VGACTL_ADDR);
   int W = inw(VGACTL_ADDR + 2);
-  // if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-    uint32_t *pixels = ctl->pixels; 
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    for (int i = 0; i < h && y + i < H; i ++) {
-      for (int j = 0; j < w && x + j < W; j ++) {
-        fb[(i + y) * W + j + x] = *pixels;
-        pixels++;
-      }
-
+  outl(SYNC_ADDR, 1);
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels; 
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int i = 0; i < h && y + i < H; i ++) {
+    for (int j = 0; j < w && x + j < W; j ++) {
+      fb[(i + y) * W + j + x] = *pixels;
+      pixels++;
     }
-  // }
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
