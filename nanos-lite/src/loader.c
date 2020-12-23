9bd6310e62bd46e15fc9b0bf34af11ed5b648505
tracer-ics2020 <tracer@njuics.org>
2020-12-23 23:32:53 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 88f324e68aa444cd91c984d705f72799   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:32:53 up 23:53,  8 users,  load average: 1.01, 1.05, 1.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,7 @@
 #include <proc.h>
 #include <elf.h>
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -10,8 +12,11 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  // MemSiz = 
+  // TODO();
+  size_t MemSiz = get_ramdisk_size();
+  size_t Offset = 1000;
+  size_t VirtAddr = 0x03000000;
+  ramdisk_write((void *)VirtAddr, Offset, MemSiz);
   return 0;
 }
 
