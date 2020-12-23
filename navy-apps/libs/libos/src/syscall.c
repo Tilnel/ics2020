628b9e182d31ea2fa22013d3fe00e9e8f3ec2d78
tracer-ics2020 <tracer@njuics.org>
2020-12-23 15:33:54 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 88f324e68aa444cd91c984d705f72799   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:33:54 up 1 day,  4:54, 10 users,  load average: 1.38, 1.19, 1.14

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,8 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  write(fd, buf, count);
-  _exit(SYS_write);
+  _syscall_(SYS_write, (intptr_t)fd, (intptr_t)buf, (intptr_t)count);
   return 0;
 }
 
