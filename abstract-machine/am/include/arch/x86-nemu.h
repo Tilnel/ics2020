2dc52f602ee14c6c9a6eb894c2cff44c919b1f0a
tracer-ics2020 <tracer@njuics.org>
2020-12-23 20:33:06 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 88f324e68aa444cd91c984d705f72799   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  20:33:06 up 20:53,  8 users,  load average: 0.94, 1.03, 1.08

--- abstract-machine/am/include/arch/x86-nemu.h
+++ abstract-machine/am/include/arch/x86-nemu.h
@@ -2,7 +2,7 @@
 #define ARCH_H__
 
 struct Context {
-  uintptr_t esp, eip, eflags;
+  // uintptr_t esp, eip, eflags;
   void *cr3;
   uintptr_t edi, esi, ebp, esp0, edx, ebx, ecx, eax;
   // uintptr_t eax, ecx, ebx, edx, esp0, ebp, esi, edi;
