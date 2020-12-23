8be0e978959b866df744aedf02a8be779295b691
tracer-ics2020 <tracer@njuics.org>
2020-12-23 13:21:15 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 88f324e68aa444cd91c984d705f72799   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:21:15 up 1 day,  2:42, 10 users,  load average: 1.43, 1.28, 1.13

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,7 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
+  assert(0);
   switch (e.event) {
     case 1: printf("A yield happens.\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
