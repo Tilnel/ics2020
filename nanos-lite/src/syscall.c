a68ca3413c8aef3374ce410e63ffab54f0c40cb6
tracer-ics2020 <tracer@njuics.org>
2021-01-08 12:52:14 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:52:15 up 14:36,  6 users,  load average: 0.99, 1.21, 1.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,7 +16,7 @@ void do_syscall(Context *c) {
     case SYS_write : c->GPRx = fs_write(a[3], (void *)a[2], a[1]); break;
     case SYS_lseek : c->GPRx = fs_lseek(a[3], a[2], a[1]); break;
     case SYS_close : c->GPRx = fs_close(a[3]); break;
-    case SYS_gettimeofday : c->GPRx = sys_gettimeofday((void *)a[3], (void *)a[2]); break;
+    case SYS_gettimeofday : c->GPRx = sys_gettimeofday((void *)0, (void *)a[2]); break;
     case SYS_brk : c->GPRx = 0; break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
