089eea2a30c3a74fc181ed713fb1f82f51af8709
tracer-ics2020 <tracer@njuics.org>
2021-01-08 16:08:40 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:08:40 up 17:52,  6 users,  load average: 1.18, 1.40, 1.39

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   // assert(file_table[fd].read == 1);
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-  ramdisk_read(buf, offset, len);
+  file_table[fd].read(buf, offset, len);
   printf("%d\n", fd);
   fs_lseek(fd, len, 1);
   return len;
