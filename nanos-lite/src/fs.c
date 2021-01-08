61ab35c0aaae212d6afad4b0d3f7be9eb22569d3
tracer-ics2020 <tracer@njuics.org>
2021-01-08 15:13:55 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:13:56 up 16:57,  6 users,  load average: 1.74, 1.62, 1.43

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,7 +16,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENT};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -33,8 +33,8 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
   [FD_EVENT]  = {"/dev/events", 0, 0, 0, events_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
