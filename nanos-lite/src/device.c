68256a5a3caa96960c894343133c481fbf2353f5
tracer-ics2020 <tracer@njuics.org>
2021-01-08 16:30:50 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:30:50 up 18:14,  6 users,  load average: 1.67, 1.53, 1.59

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,9 +21,13 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  if (ev.keycode == AM_KEY_NONE) return -1;
+  AM_INPUT_KEYBRD_T ev;
+  while(1){
+  ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) break;
   // printf("%d\n", keycode);
+  return sprintf(buf, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode], len);
+}
   return sprintf(buf, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode], len);
 }
 
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -19,7 +19,6 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   return (_read(3, buf, len) == -1) ? 0 : 1;
-  
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
