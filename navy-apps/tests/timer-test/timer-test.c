b8e4b68bcf2d088454d364ddd75fe4b43fdf9ce3
tracer-ics2020 <tracer@njuics.org>
2021-01-08 12:35:26 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:35:26 up 14:19,  6 users,  load average: 1.37, 1.28, 1.18

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -1,8 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/time.h>
-#include <time.h>
-#include <sys/_timeval.h>
 
 int main() {
   struct timeval *tv;
@@ -10,7 +8,7 @@ int main() {
   int sec = 0;
   while (1) {
 	gettimeofday(tv, tz);
-	if (sec != tv->tv_sec) printf("It's the %d's second.", sec); 
+	if (sec != tv->tv_sec) printf("It's the %d's second.\n", sec); 
 	sec = tv->tv_sec;
   }
   return 0;
