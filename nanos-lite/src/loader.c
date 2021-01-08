668fea5948c6cd31d3d01fbd05e1250c9029ff1e
tracer-ics2020 <tracer@njuics.org>
2021-01-08 10:35:43 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:35:44 up 12:19,  5 users,  load average: 1.16, 1.17, 1.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,8 +20,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // ramdisk_read(&eh, 0, sizeof(Elf_Ehdr));
   fs_read(fd, &eh, sizeof(Elf_Ehdr));
 
+  printf("%d %d\n", eh.e_entry, eh.e_phnum);
   for (int i = 0; i < eh.e_phnum; i++) {
     ramdisk_read(&ph, eh.e_phoff + i * sizeof(Elf_Phdr), sizeof(Elf_Phdr));
+    //fs_read(fd, &ph, )
     if (ph.p_type == 1) {
       ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
       memset((void *)ph.p_vaddr + ph.p_filesz, 0, ph.p_memsz - ph.p_filesz);
