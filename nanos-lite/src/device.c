1a0dff8f9cec6bcd21e0af0cc16908ceb0e37ca0
tracer-ics2020 <tracer@njuics.org>
2021-01-08 16:21:07 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  16:21:07 up 18:05,  6 users,  load average: 1.50, 1.69, 1.64

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,11 +21,10 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  int keycode = io_read(AM_INPUT_KEYBRD).keycode;
-  if (keycode == AM_KEY_NONE) { memset(buf, 0, len); return -1; }
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if (ev.keycode == AM_KEY_NONE) return 0;
   // printf("%d\n", keycode);
-  bool keydown = io_read(AM_INPUT_KEYBRD).keydown;
-  return sprintf(buf, "%s %s\n", keydown ? "kd" : "ku", keyname[keycode], len);
+  return sprintf(buf, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode], len);
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
