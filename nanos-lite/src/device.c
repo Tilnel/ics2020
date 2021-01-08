0eac8d2d82b11c89171cda0e52c827db5b4e61f2
tracer-ics2020 <tracer@njuics.org>
2021-01-08 12:36:27 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:36:28 up 14:20,  6 users,  load average: 1.37, 1.30, 1.19

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,6 +35,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
   tv->tv_sec = io_read(AM_TIMER_UPTIME).us / 1000000;
   tv->tv_usec = io_read(AM_TIMER_UPTIME).us % 1000000;
+  assert(0);
   return 0;
 }
 
