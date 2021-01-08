6e77a08f31b79e2044c84049c8b8ec312eba94da
tracer-ics2020 <tracer@njuics.org>
2021-01-08 08:10:54 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:10:54 up 22:54, 15 users,  load average: 1.42, 1.36, 1.33

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,8 +40,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int h = gpuconf.height;
   offset = fs_lseek(5, 0, 1);
   printf("%d %d ", w, h);
-  int y = offset / 4 / w;
-  int x = offset / 4 % w;
+  int y = offset / 4 / h;
+  int x = offset / 4 % h;
   printf("%d %d\n", x, y);
 
   io_write(AM_GPU_FBDRAW, x, y, (uint32_t *)buf + offset / 40, len / 4, 1, true);
