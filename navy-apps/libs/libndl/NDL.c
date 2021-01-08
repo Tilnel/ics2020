c9b904fa360d4e23e7ef0ca0d5238110cda30a49
tracer-ics2020 <tracer@njuics.org>
2021-01-08 21:00:27 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:00:28 up 22:44, 15 users,  load average: 1.12, 1.23, 1.27

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -54,7 +54,8 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int fd = _open("/dev/fb", 0, 0);
   for (int i = 0; i < h; i++) {
-    lseek(fd, ((y - i) * screen_w + x) * 4, 0);
+    int off = lseek(fd, ((y - i) * screen_w + x) * 4, 0);
+    printf("%d\n", off);
     write(fd, (void *)pixels, w * 4);
   }
 }
