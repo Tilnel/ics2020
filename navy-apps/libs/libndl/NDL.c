fffb91f950cdb5437712901b0c44d80365968848
tracer-ics2020 <tracer@njuics.org>
2021-01-08 15:26:58 +0800

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:26:58 up 17:10,  6 users,  load average: 1.24, 1.43, 1.44

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,8 +18,8 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  _read(3, buf, len);
-  return 0;
+  _read(100, buf, len);
+  return 1;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
