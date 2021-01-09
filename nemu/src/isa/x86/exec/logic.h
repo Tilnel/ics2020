71f307b6e7322984158c8e509e7dbdd29f75686e
tracer-ics2020 <tracer@njuics.org>
2021-01-09 03:00:30 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:00:30 up 1 day, 17:44, 17 users,  load average: 1.87, 1.58, 1.43

--- nemu/src/isa/x86/exec/logic.h
+++ nemu/src/isa/x86/exec/logic.h
@@ -25,14 +25,7 @@ static inline def_EHelper(andl) {
 static inline def_EHelper(xor) {
   *s0 = *ddest ^ *dsrc1;
   cpu.CF = cpu.OF = 0;
-  cpu.CF = 1;
-  printf("set1 efl %d\n", cpu.eflags);
-  cpu.CF = 0;
-  printf("set0 efl %d\n", cpu.eflags);
-  cpu.CF = 1;
-  printf("set1 efl %d\n", cpu.eflags);
   rtl_update_ZFSF(s, s0, id_dest->width);
-  printf("efl %d\n", cpu.eflags);
   //TODO();
   operand_write(s, id_dest, s0);
   print_asm_template2(xor);
