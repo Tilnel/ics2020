a76a2ac245e14dc4e008262cf6a5b9d945b7964d
tracer-ics2020 <tracer@njuics.org>
2021-01-09 03:16:25 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:16:25 up 1 day, 18:00, 17 users,  load average: 1.66, 1.75, 1.72

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -8,21 +8,43 @@ static const char *keyname[] = {"NONE", _KEYS(keyname)};
 
 int SDL_PushEvent(SDL_Event *ev) { return 0; }
 
-int SDL_PollEvent(SDL_Event *ev) { return 0; }
-
-int SDL_WaitEvent(SDL_Event *event) {
+int SDL_PollEvent(SDL_Event *ev) {
     char buf[64];
     char state[2];
     char name[20];
     NDL_PollEvent(buf, 64);
-    if (buf[0] == 0) return 0;
+    if (buf[0] == 0)
+        return 0;
     sscanf(buf, "%s %s\n", state, name);
     event->key.type = event->type = (state[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
-    for (int i = 0; i <83; i++) {
+    for (int i = 0; i < 83; i++) {
         if (!strcmp(keyname[i], name)) {
             event->key.keysym.sym = i;
             break;
         }
+    }
+
+    return 1;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+    while (1) {
+        char buf[64];
+        char state[2];
+        char name[20];
+        NDL_PollEvent(buf, 64);
+        if (buf[0] == 0)
+            continue;
+        sscanf(buf, "%s %s\n", state, name);
+        event->key.type = event->type =
+            (state[1] == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+        for (int i = 0; i < 83; i++) {
+            if (!strcmp(keyname[i], name)) {
+                event->key.keysym.sym = i;
+                break;
+            }
+        }
+        return 1;
     }
 
     return 1;
