8a957e22854b80b768870ec5ad0c8d91eb1a3d17
tracer-ics2020 <tracer@njuics.org>
2021-01-08 21:47:52 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:47:52 up 1 day, 12:31, 15 users,  load average: 1.65, 1.55, 1.44

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -16,6 +16,7 @@ int SDL_WaitEvent(SDL_Event *event) {
     char name[20];
     NDL_PollEvent(buf, 64);
     sscanf(buf, "k%c %s\n", state, name);
+    printf("%s\n", buf);
     printf("%c\n", state);
     event->key.type = event->type = (state == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
     for (int i = 0; i <83; i++) {
