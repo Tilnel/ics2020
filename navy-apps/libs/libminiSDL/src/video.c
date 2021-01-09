ba6731f48496c404138a1cd312e73fde78b8c593
tracer-ics2020 <tracer@njuics.org>
2021-01-09 08:05:31 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 8a7ca102967e49bb953335478f16516e   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:05:31 up  2:10,  1 user,  load average: 1.05, 1.08, 0.94

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -32,8 +32,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst,
     int xd = dstrect->x;
     int yd = dstrect->y;
     // printf("%d\n", (int)dstrect);
-    // printf("%d %d %d %d %d %d %d %d %d %d %d %d\n", Ws, Hs, Wd, Hd, xs, ys,
-    // ws, hs, xd, yd, wd, hd);
+    printf("%d %d %d %d %d %d %d %d %d %d %d %d\n", Ws, Hs, Wd, Hd, xs, ys, ws, hs, xd, yd, wd, hd);
     if (src->format->BytesPerPixel == 4) {
         for (int i = 0; i < hs; i++) {
             for (int j = 0; j < ws; j++) {
