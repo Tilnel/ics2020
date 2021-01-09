8e5f5d789d3505fc2e1619e0c908961aedbdc3ff
tracer-ics2020 <tracer@njuics.org>
2021-01-08 22:18:39 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:18:39 up 1 day, 13:02, 15 users,  load average: 1.38, 1.44, 1.35

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -6,8 +6,8 @@
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst,
                      SDL_Rect *dstrect) {
-    assert(dst && src);
-    assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+    // assert(dst && src);
+    // assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
     int Ws = src->w;
     int Hs = src->h;
     int Wd = dst->w;
