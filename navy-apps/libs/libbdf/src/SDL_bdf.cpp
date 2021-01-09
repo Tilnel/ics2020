b45af3484aa03eaeff7bff9c24d31b78c6b627aa
tracer-ics2020 <tracer@njuics.org>
2021-01-08 23:59:38 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  23:59:38 up 1 day, 14:43, 15 users,  load average: 1.67, 1.36, 1.38

--- navy-apps/libs/libbdf/src/SDL_bdf.cpp
+++ navy-apps/libs/libbdf/src/SDL_bdf.cpp
@@ -6,6 +6,7 @@ SDL_Surface* BDF_CreateSurface(BDF_Font *font, char ch, uint32_t fg, uint32_t bg
   uint32_t *bm = font->font[ch];
   if (!bm) return NULL;
   int w = font->w, h = font->h;
+  assert(w);
   uint32_t *pixels = (uint32_t *)malloc(w * h * sizeof(uint32_t));
   assert(pixels);
   for (int j = 0; j < h; j ++) {
