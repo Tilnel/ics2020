4a19ff35a67dcea4e16054ebf6aa7f0e787ec3f1
tracer-ics2020 <tracer@njuics.org>
2021-01-09 05:46:58 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:46:58 up 1 day, 20:30, 17 users,  load average: 1.07, 1.12, 1.22

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -67,7 +67,6 @@ int fs_open(const char *pathname, int flags, int mode) {
 size_t fs_read(int fd, void *buf, size_t len) {
   // assert(file_table[fd].read == 1);
   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-  printf("fd%d\n", fd);
   int ret = file_table[fd].read(buf, offset, len);
   fs_lseek(fd, len, 1);
   return ret;
