4185b9b3ed1eb43d6d2401fee672b9cf8248b7b4
tracer-ics2020 <tracer@njuics.org>
2021-01-08 21:29:24 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  21:29:24 up 1 day, 12:13, 15 users,  load average: 1.83, 1.42, 1.27

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,29 +1,34 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
-static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+static const char *keyname[] = {"NONE", _KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
-}
+int SDL_PushEvent(SDL_Event *ev) { return 0; }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
-}
+int SDL_PollEvent(SDL_Event *ev) { return 0; }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+    char buf[64];
+    char state;
+    char name[20];
+    NDL_PollEvent(buf, 64);
+    sscanf(buf, "k%c %s\n", state, name);
+    event->key.type = event->type = (state == 'd') ? SDL_KEYDOWN : SDL_KEYUP;
+    for (int i = 0; i <128; i++) {
+        if (!strcmp(keyname[i], name)) {
+            event->key.keysym = (SDL_keysym){i};
+            break;
+        }
+    }
+
+    return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  return 0;
+    return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
-}
+uint8_t *SDL_GetKeyState(int *numkeys) { return NULL; }
