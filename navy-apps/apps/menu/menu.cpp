dd85ab980ab18585304ed4d9f47aad780deacc78
tracer-ics2020 <tracer@njuics.org>
2021-01-09 01:30:48 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 5b80657a08aa408081fef2c3cbc87f1b   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  01:30:48 up 1 day, 16:14, 17 users,  load average: 1.26, 1.36, 1.31

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -68,7 +68,7 @@ static void clear_display(void) {
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-  printf("RESULTOFCMP%d\n", strcmp("ENDPROPERTIES", "ENDFONT"));
+  printf("RESULTOFCMP%d\n", strcmp("BNDPROPERTIES", "ENDFONT"));
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
 
