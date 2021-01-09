d7a325ae3c116367fcb4ae3d08c80589aa4fe228
tracer-ics2020 <tracer@njuics.org>
2021-01-09 12:43:14 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 8a7ca102967e49bb953335478f16516e   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:43:14 up  6:48,  1 user,  load average: 1.06, 1.39, 1.40

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -88,7 +88,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
             for (int i = 0; i < h; i++) {
                 for (int j = 0; j < w; j++) {
                     pixelbuf[i * w + j] = 
-                        s->format->palette->colors[(uint8_t)(s->pixels[i * w + j])].val;
+                        s->format->palette->colors[*(uint8_t *)(s->pixels + i * w + j)].val;
                 }
             }
             // ConvertPixelsARGB_ABGR(pixelbuf, pixelbuf, w * h);
