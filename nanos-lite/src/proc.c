eb9124cdcafdbf81697fc854c95a706623fccab4
tracer-ics2020 <tracer@njuics.org>
2021-01-11 09:00:21 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3f66ab191a774046971c5e8f13a82181   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  09:00:22 up  2:35,  1 user,  load average: 1.30, 0.93, 0.73

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,8 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/nterm");//pal/pal-x86");
+  naive_uload(NULL, "/bin/pal/pal-x86");
+  // naive_uload(NULL, "/bin/nterm");//pal/pal-x86");
 
 }
 
