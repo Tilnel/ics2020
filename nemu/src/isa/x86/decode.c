ca909e1b54240fb73d88a28430f60f4a5a98724a
tracer-ics2020 <tracer@njuics.org>
2021-01-13 11:23:48 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  11:23:48 up 23:55,  1 user,  load average: 1.58, 0.91, 0.72

--- nemu/src/isa/x86/decode.c
+++ nemu/src/isa/x86/decode.c
@@ -105,7 +105,6 @@ void read_ModR_M(DecodeExecState *s, Operand *rm, bool load_rm_val, Operand *reg
   if (m.mod == 3) operand_reg(s, rm, load_rm_val, m.R_M, rm->width);
   else {
     load_addr(s, &m, rm);
-    if(s->opcode == 0x03) printf("width%d\n", rm->width);
     if (load_rm_val) rtl_lm(s, &rm->val, s->isa.mbase, s->isa.moff, rm->width);
     rm->preg = &rm->val;
   }
