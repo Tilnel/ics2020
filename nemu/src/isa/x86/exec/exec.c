302beec5024608b21a6dc1bed40c1cc368ae41ca
tracer-ics2020 <tracer@njuics.org>
2021-01-13 10:18:51 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  10:18:51 up 22:50,  1 user,  load average: 0.31, 0.42, 0.47

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -211,7 +211,7 @@ again:
     IDEX (0x68, push_I, push)
     IDEX (0x69, I_E2G, imul3)
     IDEXW(0x6a, push_I, push_imm8, 1)
-	  IDEX (0x6b, I_E2G, imul3) 
+	  IDEXW(0x6b, I_E2G, imul3, 1) 
     IDEXW(0x70, J, jcc, 1)
     IDEXW(0x71, J, jcc, 1)
     IDEXW(0x72, J, jcc, 1)
