9f37df8929f01d12f1fcddda9f8ee306e619bcc9
tracer-ics2020 <tracer@njuics.org>
2021-01-19 13:12:45 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:12:46 up 7 days,  1:43,  1 user,  load average: 1.65, 1.38, 1.28

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -27,10 +27,10 @@ static void sh_handle_cmd(const char *cmd) {
   int len = strlen(cmd);
   char *argv[5];
   sscanf(cmd, "%s %s %s %s %s", buf, argv[1], argv[2], argv[3], argv[4]);
+  printf("%s %s %s %s %s\n", argv[0], argv[1], argv[2], argv[3], argv[4]);
   assert(0);
   // printf("%s", buf);
   sprintf(argv[0], "%s", buf);
-  printf("%s %s %s %s %s\n", argv[0], argv[1], argv[2], argv[3], argv[4]);
   setenv("PATH", "/bin", 1);
   execvp(buf, argv);
 }
