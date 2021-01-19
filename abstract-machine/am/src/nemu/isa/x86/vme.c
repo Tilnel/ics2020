fe93794c0664b398fdfd591d73eb304abce9499d
tracer-ics2020 <tracer@njuics.org>
2021-01-19 12:36:30 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  12:36:30 up 7 days,  1:07,  1 user,  load average: 1.16, 1.23, 1.18

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -61,9 +61,9 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 
 Context* ucontext(AddrSpace *as, Area kstack, void *entry, char *const argv[], char *const envp[]) {
   Context *ret = as->area.end - 64;
-  int i = 1;
-  while (argv[i++] != NULL);
-  int argc = i - 1;
+  // int i = 1;
+  // while (argv[i++] != NULL);
+  int argc = 1;
   // printf("argc %d\n", argc);
   ret->eip = (int)entry;
   ret->esp = (int)(ret);
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,9 +26,9 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-    // static char *argvv[] = {"/bin/pal"};
+    static char *argvv[] = {"/bin/nterm"};
     // context_kload(&pcb[0], hello_fun, "abc");
-    context_uload(&pcb[0], "/bin/nterm", NULL, NULL);
+    context_uload(&pcb[0], "/bin/nterm", argvv, NULL);
     switch_boot_pcb();
 
     Log("Initializing processes...");
