aa6726c1444c179761ed701cf4228c36e4fe0dfa
tracer-ics2020 <tracer@njuics.org>
2021-01-19 07:24:01 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  07:24:01 up 6 days, 19:55,  1 user,  load average: 1.40, 1.28, 1.14

--- abstract-machine/am/src/nemu/isa/x86/cte.c
+++ abstract-machine/am/src/nemu/isa/x86/cte.c
@@ -57,7 +57,7 @@ Context* kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *ret = (void *)(kstack.end - 100);
   ret->eip = (int)entry;
   ret->esp = (int)(ret);
-  ((uint32_t *)ret)[14] = (int)arg;
+  ((uint32_t *)ret)[13] = (int)arg;
   return ret;
 }
 
