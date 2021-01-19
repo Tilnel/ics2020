8ff5d6ab283c50d4961799ebf29e26bbab74781c
tracer-ics2020 <tracer@njuics.org>
2021-01-19 13:55:32 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  13:55:32 up 7 days,  2:26,  1 user,  load average: 1.67, 1.21, 1.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,9 +26,9 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-    static char *argvv[] = {"/bin/busybox"};
+    static char *argvv[] = {"/bin/nterm"};
     // context_kload(&pcb[0], hello_fun, "abc");
-    context_uload(&pcb[0], "/bin/busybox", argvv, NULL);
+    context_uload(&pcb[0], "/bin/nterm", argvv, NULL);
     switch_boot_pcb();
 
     Log("Initializing processes...");
