f5b40886d778bff30db3c3db2dd1d49acbacf9cb
tracer-ics2020 <tracer@njuics.org>
2021-01-19 08:57:44 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 93f57cda79c9454e8d1ea41f232b2689   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  08:57:44 up 6 days, 21:28,  1 user,  load average: 1.04, 1.24, 1.19

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -66,8 +66,8 @@ Context* ucontext(AddrSpace *as, Area kstack, void *entry, char *const argv[], c
   printf("argc %s\n", argv[1]);
   ret->eip = (int)entry;
   ret->esp = (int)(ret);
-  ((uint32_t *)ret)[14] = argc;  // under stack 1 byte
-  ((uint32_t *)ret)[15] = (int)argv;  
-  ((uint32_t *)ret)[16] = (int)envp;  
+  ((uint32_t *)ret)[15] = argc;  // under stack 1 byte
+  ((uint32_t *)ret)[16] = (int)argv;  
+  ((uint32_t *)ret)[17] = (int)envp;  
   return ret;
 }
