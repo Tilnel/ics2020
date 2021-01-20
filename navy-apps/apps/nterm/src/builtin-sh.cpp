6e64b6646883002634b0f640b519fef7a280103d
tracer-ics2020 <tracer@njuics.org>
2021-01-20 05:05:39 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:05:40 up  1:10,  2 users,  load average: 0.36, 0.47, 0.47

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -29,6 +29,7 @@ static void sh_handle_cmd(const char *cmd) {
   strcpy(buf, cmd);
   for (int i = 0; i < 5; i++) {
     argv[i] = strtok(buf, buf);
+    printf("%s\n", argv[i]);
   }
   // printf("%s\n", argv[1]);
   // printf("%s", buf);
