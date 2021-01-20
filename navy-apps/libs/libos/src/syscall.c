525a9fbd3abdcbef5bd80ccff08158842cc674c2
tracer-ics2020 <tracer@njuics.org>
2021-01-20 17:52:06 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  17:52:06 up 13:56,  2 users,  load average: 0.99, 0.66, 0.58

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <assert.h>
 #include <time.h>
+#include <stdio.h>
 #include "syscall.h"
 
 // helper macros
@@ -94,6 +95,7 @@ int _gettimeofday(struct timeval *tv, struct timezone *tz) {
 
 extern int errno;
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
+  printf("%s\n%s\n", argv[0], argv[1]);
   int ret = _syscall_(SYS_execve, (intptr_t)fname, (intptr_t)argv, (intptr_t)envp);
   if (ret < 0) errno = -ret;
   return -1;
