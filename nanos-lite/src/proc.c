1e4b2b0602d19bdb8ca57b1f07f4dec9882a2821
tracer-ics2020 <tracer@njuics.org>
2021-01-21 02:37:09 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  02:37:09 up 22:41,  2 users,  load average: 0.68, 0.53, 0.49

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -38,7 +38,7 @@ void init_proc() {
 
 Context *schedule(Context *prev) {
     current->cp = prev;
-    current = (current == pcb)? pcb + 1 : pcb;
+    current = (current == &pcb[0])? &pcb[1] : &pcb[0];
     return current->cp;
 }
 
