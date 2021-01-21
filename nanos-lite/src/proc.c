460c4573707c65a8c54f8acf41361be097a84b9b
tracer-ics2020 <tracer@njuics.org>
2021-01-21 03:45:17 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:45:17 up 23:49,  2 users,  load average: 0.42, 0.46, 0.54

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,10 +26,13 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-    static char *argvv[] = {"/bin/nterm"};
+    // static char *argvv[] = {"/bin/nterm"};
+    // context_uload(&pcb[1], "/bin/nterm", argvv, NULL);
+
+    static char *argvv[] = {"/bin/pal", "--skip"};
+    context_uload(&pcb[1], "/bin/pal", argvv, NULL);
 
     context_kload(&pcb[0], hello_fun, "abc");
-    context_uload(&pcb[1], "/bin/nterm", argvv, NULL);
 
     switch_boot_pcb();
 
