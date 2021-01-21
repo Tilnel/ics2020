352746dfe86940f962239d2f7ec38a053bb43b90
tracer-ics2020 <tracer@njuics.org>
2021-01-21 03:12:57 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:12:58 up 23:17,  2 users,  load average: 0.40, 0.51, 0.62

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,7 +11,7 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit : c->GPRx = sys_execve("/bin/nterm", NULL, NULL); break;
+    case SYS_exit : c->GPRx = sys_execve("/bin/menu", NULL, NULL); break;
     case SYS_yield : yield(); break;
     case SYS_open : c->GPRx = fs_open((char *) a[3], a[2], a[1]); break;
     case SYS_read : c->GPRx = fs_read(a[3], (void *)a[2], a[1]); break;
