c685735a4f48b5a7a3a61ed90481b66c46eae462
tracer-ics2020 <tracer@njuics.org>
2021-01-21 04:08:15 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:08:15 up 1 day, 12 min,  2 users,  load average: 0.51, 0.57, 0.58

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -14,7 +14,7 @@ extern char **environ;
 
 void call_main(int argc, char *argv[], char *envp[]) {
   printf("argc %d\n", argc);
-  printf("arg1 %x\n", (*((int *)&argc + 1)));
-  printf("argv0 %x\n", &argv[0]);
+  printf("argv0 %s\n", (char *)&argv[0]);
+  // char* nargv[] = {&argv[0], &argv[1], &arg}
   exit(main(argc, argv, envp));
 }
