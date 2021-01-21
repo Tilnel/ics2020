ecb6578fcc58674745a979b85e368e3432ab5a84
tracer-ics2020 <tracer@njuics.org>
2021-01-21 18:08:03 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  18:08:03 up 1 day, 14:12,  4 users,  load average: 1.23, 0.68, 0.49

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -7,11 +7,13 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
   uint32_t offset = vaddr & 0xfff;
 
   paddr_t cr3 = cpu.cr3;
-  printf("%x\n", cr3);
   paddr_t page_sheet = paddr_read(cr3 + 4 * dir, 4);
-  if ((page_sheet & 1) == 0) panic("pagefault at vaddr = 0x%x", vaddr);
+  if ((page_sheet & 1) == 0) {
+    panic("pagefault at vaddr = 0x%x, pagesheet %x", vaddr, page_sheet);
+  }
   paddr_t page_sheet_item = paddr_read((page_sheet & 0xfffff000) + 4 * page, 4);
-  if ((page_sheet_item & 1) == 0) panic("pagefault at vaddr = 0x%x", vaddr);
+  if ((page_sheet_item & 1) == 0) 
+    panic("pagefault at vaddr = 0x%x, pagesheet %x", vaddr, page_sheet);
   paddr_t paddr = (page_sheet_item & 0xfffff000) + offset;
   
   // panic("cross page at vaddr = 0x%x, paddr = 0x%x", vaddr, paddr);
