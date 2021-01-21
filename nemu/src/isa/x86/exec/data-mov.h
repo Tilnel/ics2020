0a45be189377dc6fc41db5458beaf78f9039949b
tracer-ics2020 <tracer@njuics.org>
2021-01-21 15:35:20 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:35:20 up 1 day, 11:39,  4 users,  load average: 0.75, 0.70, 0.65

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -126,10 +126,9 @@ static inline def_EHelper(xchg) {
 }
 
 static inline def_EHelper(r2cr) {
-  printf("cr0 %x", cpu.cr0);
-  if (s->opcode == 0x20) assert(0);
+  printf("cr0 %x\n", cpu.cr0);
   *dsrc1 = *ddest;
-  printf("cr0 %x", cpu.cr0);
+  printf("cr0 %x\n", cpu.cr0);
   assert(0);
 }
 
