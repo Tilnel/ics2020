919c153fd45d1fc2516634c3d8faa93896b8d121
tracer-ics2020 <tracer@njuics.org>
2021-01-21 04:18:00 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  04:18:00 up 1 day, 22 min,  2 users,  load average: 0.69, 0.62, 0.61

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -64,7 +64,7 @@ void setargs(PCB *p, const char *filename, char *const argv[], char *const envp[
         ((uintptr_t *)args)[i] = (uintptr_t)str + pos;
         printf("arg%d:  %x\n", i, ((uintptr_t *)args)[i]);
         printf("arg%d: %s\n", i, (void *)((uintptr_t *)args)[i]);
-        pos += strlen(argv[i] + 1);
+        pos += strlen(argv[i]) + 1;
     }
     uintptr_t pargc = (uintptr_t)args - 4;
     ((uint32_t *)pargc)[0] = argc;
