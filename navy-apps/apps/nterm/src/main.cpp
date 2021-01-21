cce795d85af624db47bf4b8f26b43a7fa0ae4c50
tracer-ics2020 <tracer@njuics.org>
2021-01-21 03:19:08 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  03:19:08 up 23:23,  2 users,  load average: 1.01, 0.75, 0.69

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
   screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
   term = new Terminal(W, H);
 
-  if (argc < 2) { builtin_sh_run(); assert(0); }
+  if (argc < 2) { assert(0);builtin_sh_run(); }
   else { extern_app_run(argv[1]); }
   // should not reach here
   assert(0);
