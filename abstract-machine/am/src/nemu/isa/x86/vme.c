b8acdec567f3e4ed510d8dfb18b1404dfb4a72b9
tracer-ics2020 <tracer@njuics.org>
2021-01-21 15:23:52 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  15:23:52 up 1 day, 11:28,  4 users,  load average: 0.94, 0.83, 0.62

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -27,10 +27,10 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   }
 
   set_cr3(kas.ptr);
-  assert(0);
   set_cr0(get_cr0() | CR0_PG);
   vme_enable = 1;
 
+  assert(0);
   return true;
 }
 
