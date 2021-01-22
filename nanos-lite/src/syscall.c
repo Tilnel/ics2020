5c6553e92dfcded9d61f28855edbe6ff04df3932
tracer-ics2020 <tracer@njuics.org>
2021-01-21 22:48:01 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  22:48:01 up 1 day, 18:52,  4 users,  load average: 0.91, 0.50, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,7 @@
 #include "syscall.h"
 extern void naive_uload(void *pcb, const char *filename);
 int sys_execve(const char *filename, char *const argv[], char *const envp[]);
+int mm_brk(uintptr_t brk);
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -11,7 +12,7 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit : halt(a[1]);//c->GPRx = sys_execve("/bin/nterm", NULL, NULL); break;
+    case SYS_exit : halt(a[3]);//c->GPRx = sys_execve("/bin/nterm", NULL, NULL); break;
     case SYS_yield : yield(); break;
     case SYS_open : c->GPRx = fs_open((char *) a[3], a[2], a[1]); break;
     case SYS_read : c->GPRx = fs_read(a[3], (void *)a[2], a[1]); break;
@@ -19,7 +20,7 @@ void do_syscall(Context *c) {
     case SYS_lseek : c->GPRx = fs_lseek(a[3], a[2], a[1]); break;
     case SYS_close : c->GPRx = fs_close(a[3]); break;
     case SYS_gettimeofday : c->GPRx = sys_gettimeofday((void *)a[3], (void *)a[2]); break;
-    case SYS_brk : c->GPRx = 0; break;
+    case SYS_brk : c->GPRx = mm_brk(a[3]); break;
     case SYS_execve : //printf("%s\n%s\n", ((char **)a[2])[0], ((char **)a[2])[1]);
       c->GPRx = sys_execve((void *)a[3], (void *)a[2], (void *)a[1]); break;
 
