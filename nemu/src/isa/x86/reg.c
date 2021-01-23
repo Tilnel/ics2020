04bb4cbd084487f252ae41ad2ef70b6c783b2528
tracer-ics2020 <tracer@njuics.org>
2021-01-23 05:47:01 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:47:01 up 3 days,  1:51,  4 users,  load average: 0.51, 0.74, 0.69

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -49,10 +49,10 @@ void isa_reg_display() {
   printf("esp		0x%x\n", cpu.esp);
   printf("ebp		0x%x\n", cpu.ebp);
   printf("esi		0x%x\n", cpu.esi);
-  printf("edi             0x%x\n", cpu.edi);
-  printf("pc              0x%x\n", cpu.pc );
-  printf("cr3             0x%x\n", cpu.cr3);
-  printf("ss              0x%x\n", cpu.ss );
+  printf("edi           0x%x\n", cpu.edi);
+  printf("pc            0x%x\n", cpu.pc );
+  printf("cr3           0x%x\n", cpu.cr3);
+  printf("ss            0x%x\n", cpu.ss );
   return;
 }
 
