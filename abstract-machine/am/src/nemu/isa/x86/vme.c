f49b2c67150da524dfa85e6ed4c5588ec19586b1
tracer-ics2020 <tracer@njuics.org>
2021-01-23 05:22:17 -0500

    >  run 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  05:22:17 up 3 days,  1:26,  4 users,  load average: 0.67, 0.60, 0.38

--- abstract-machine/am/src/nemu/isa/x86/vme.c
+++ abstract-machine/am/src/nemu/isa/x86/vme.c
@@ -82,6 +82,7 @@ Context* ucontext(AddrSpace *as, Area kstack, void *entry) {
   ret->cs = USEL(3);
   ret->ss3 = USEL(4);
   ret->cr3 = as->ptr;
+  printf("%ret cr3 %x\n", ret->cr3);
   ret->eflags |= 0x200;
   return ret;
 }
