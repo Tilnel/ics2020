005b1415ffddca6877102a2f8eb414db7e3218a6
tracer-ics2020 <tracer@njuics.org>
2021-01-23 14:57:56 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  14:57:56 up 3 days, 11:02,  4 users,  load average: 0.85, 0.61, 0.39

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -2,13 +2,13 @@
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
 paddr_t isa_mmu_translate(vaddr_t vaddr, int type, int len) {
-  if (vaddr > 0x101825 && vaddr < 0x101827) printf("%x %x\n", vaddr, cpu.cr3);
   uint32_t dir = vaddr >> 22;
   uint32_t page = (vaddr >> 12) & 0x3ff;
   uint32_t offset = vaddr & 0xfff;
 
   paddr_t cr3 = cpu.cr3;
   paddr_t page_sheet = paddr_read(cr3 + 4 * dir, 4);
+  if (vaddr > 0x101825 && vaddr < 0x101827) printf("%x %x %x\n", vaddr, cpu.cr3, page_sheet);
   if ((page_sheet & 1) == 0) 
     panic("pagefault at pc = 0x%x, vaddr = 0x%x, pagesheet %x", cpu.pc, vaddr, page_sheet);
   paddr_t page_sheet_item = paddr_read((page_sheet & 0xfffff000) + 4 * page, 4);
