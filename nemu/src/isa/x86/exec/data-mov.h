9ec8c210a4ccc7b5a8a87914fdf31b62028c98c5
tracer-ics2020 <tracer@njuics.org>
2021-01-23 06:00:40 -0500

    >  compile 191240046    Static hostname: Tilnel          Icon name: computer-laptop            Chassis: laptop         Machine ID: a5a1d8707eb248e987a5be3f95eb31be            Boot ID: 3cbf191356e4438d8403e2bd9f831910   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-10-amd64       Architecture: x86-64  06:00:40 up 3 days,  2:05,  4 users,  load average: 0.33, 0.41, 0.54

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -130,7 +130,7 @@ static inline def_EHelper(r2cr) {
   printf("cr3, eax, pc: %x %x %x\n", cpu.cr3, cpu.eax, cpu.pc);
   *dsrc1 = *ddest;
   if (ddest == &cpu.eax && dsrc1 == &cpu.cr3)
-  printf("after mov %%cr3, %%eax: cr3, eax: %x %x\n", cpu.cr3, cpu.eax);
+  printf("after mov %%eax, %%cr3: cr3, eax: %x %x\n", cpu.cr3, cpu.eax);
 }
 
 static inline def_EHelper(cr2r) {
